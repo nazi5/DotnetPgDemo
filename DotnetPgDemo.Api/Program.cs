b898fe004c35dcf7c43fca1d34a6d7aecfeaca4c
Nazi Shaik <shaiknazi717@gmail.com>
2026-06-15 17:26:19 +0300

    Added swagger

--- DotnetPgDemo.Api/Program.cs
+++ DotnetPgDemo.Api/Program.cs
@@ -11,8 +11,19 @@ builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
-
+builder.Services.AddOpenApi();
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    // Generates the openapi.json file
+    app.MapOpenApi(); 
+    
+    // Creates the visual UI and points it to the generated json file
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My API Documentation");
+    });
+}
 app.MapControllers();
 
 app.Run();
